b04f825becbf883cd058fdad47f5a9fda29a1984
Adam Ghaoul <adamghaoul@gmail.com>
2021-07-24 17:15:29 +0200

    css done

--- client/src/components/pages/game.js
+++ client/src/components/pages/game.js
@@ -1,32 +1,30 @@
-import React from 'react';
-import Banner from '../banner/banner'
-import GameArea from '../game-area/gameArea'
-import Assistant from "../assistant/assistant"
-import Menu from "../menu/menu"
-import GameEvents from "../game-events/game-events"
-import Logout from "../logout-btn"
-
+import React from "react";
+import Banner from "../banner/banner";
+import GameArea from "../game-area/gameArea";
+import Assistant from "../assistant/assistant";
+import Menu from "../menu/menu";
+import GameEvents from "../game-events/game-events";
+import Logout from "../logout-btn";
 
 function Game() {
+  return (
+    <div className="globalContainer">
+      <div className="first">
+        <Banner />
+        <Logout />
+      </div>
+      <div className="second">
+        <Menu />
+       <GameArea />
+        <GameEvents />  
+        <Assistant />
+      </div>
 
-  return (<div className="globalContainer">
-
-    <div id="top">
-      <Banner />
-      <Logout />
+      {/* <div className="third">
+ 
+      </div> */}
     </div>
-    <div id="middle">
-      <Menu />
-      <GameArea />
-      <GameEvents />
-    </div>
-    <div id="bottom">
-      <Assistant />
-    </div>
-
-
-  </div>)
-
+  );
 }
 
-export default Game
+export default Game;
