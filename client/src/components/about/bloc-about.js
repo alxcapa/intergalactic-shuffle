42120d08c03c09d047a3506fdd84deb1ca8fd127
Projetphp <alexandre.capaldi@hotmail.fr>
2021-08-12 23:20:49 +0200

    about photo

--- client/src/components/about/bloc-about.js
+++ client/src/components/about/bloc-about.js
@@ -13,13 +13,13 @@ function BlocAbout() {
       </div>
       <br />
       <div>
-      Up to you to save humanity, Robo-san wants to see your best moves or else the earth will be destroyed
-      <br/> <br/>
-       Grab as many planets as possible in the time limit.
+        Up to you to save humanity, Robo-san wants to see your best moves or else the earth will be destroyed
+      <br /> <br />
+        Grab as many planets as possible in the time limit.
       </div>
       <br />
       <div>
-    Check your profile, stats and leaderboard and see, your score, who the best dancer is and where they're from ! 
+        Check your profile, stats and leaderboard and see, your score, who the best dancer is and where they're from !
       </div>
       <br />
       <div>
@@ -28,7 +28,7 @@ function BlocAbout() {
       </div>
       <br />
       <div>
-        <img src="images/alex-linkedin.jpeg"></img><img src="images/lnkdin.png"></img>
+        <img src="images/alex-linkedin.jpeg"></img><img src="images/lnkdin.png"></img><img src="images/adam-profil.jpg"></img>
 
       </div>
       <br />
